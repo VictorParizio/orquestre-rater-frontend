64e7dfdadde0e450c22b2c36f06a223c01076f05
Victor Parizio <victorparizio@hotmail.com>
2024-08-12 23:31:41 -0300

    feat: implement modal components

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import { GlobalStyle } from "./styles/GlobalStyle";
 import { Header } from "./components/Header";
 import { Home } from "./pages/Home";
 import { Footer } from "./components/Footer";
+import { Login } from "./components/Login";
 import { Signup } from "./components/Signup";
+import { UpdateUser } from "./components/UpdateUser";
 
 export const App = () => {
   return (
@@ -11,7 +13,9 @@ export const App = () => {
       <Header />
       <Home />
       <Footer />
+      <Login />
       <Signup />
+      <UpdateUser />
     </>
   );
 };
