1fb1524b40cfaef21501992cbf7a95f7dca74be9
Victor Parizio <victorparizio@hotmail.com>
2024-08-14 05:51:38 -0300

    feat: recommendations movies carousel

--- src/components/MovieCarousel/RecommendationsCarousel.tsx
+++ src/components/MovieCarousel/RecommendationsCarousel.tsx
@@ -1,13 +1,38 @@
 import { MovieCard } from "src/components/MovieCard";
 import { Carousel } from "../Carousel";
+import { useEffect, useState } from "react";
+import { getAPI } from "src/http/movies";
+import { Movie } from "src/pages/Home";
+
+export interface ApiResponseMovies {
+  results: Movie[];
+}
 
 export const RecommendationsCarousel = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    (async function () {
+      try {
+        const response: ApiResponseMovies = await getAPI(
+          `533535/${"recommendations"}`
+        );
+        setMovies(response.results);
+      } catch (error: unknown) {
+        console.error("Erro ao carregar lista de lançamentos", error);
+      }
+    })();
+  }, []);
   return (
     <Carousel title="Recomendações">
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
+      {movies.map((movie) => (
+        <MovieCard
+          key={movie.id}
+          backdrop_path={movie.backdrop_path}
+          title={movie.title}
+          vote_average={movie.vote_average.toFixed(1)}
+        />
+      ))}
     </Carousel>
   );
 };
